43f12c3d56242eef82f532631a22a95791a02a57
Breyhan-F9 <breyhan20@gmail.com>
2024-04-28 17:44:30 +0700

    working link in dashboard

--- frontend/app/(admin)/dashboard/Dashboard.jsx
+++ frontend/app/(admin)/dashboard/Dashboard.jsx
@@ -53,13 +53,13 @@ function Dashboard() {
                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
          </button>
-        <a href="https://flowbite.com" class="flex ms-2 md:me-24">
+        <Link href="/dashboard" class="flex ms-2 md:me-24">
          <Image 
          className="w-16 h-10 mr-2.5" 
          src={logo} 
          />
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap ">Nettoyer</span>
-        </a>
+        </Link>
       </div>
       <div class="flex items-center">
           <div class="flex items-center ms-3">
