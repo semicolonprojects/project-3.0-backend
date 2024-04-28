67d95c952a5bdf4d402386bfbab615a22d9c0cf9
Fikkie007 <fikkieakbar48@gmail.com>
2024-04-28 23:48:39 +0700

    Create Delete Product

--- frontend/app/(admin)/login/page.jsx
+++ frontend/app/(admin)/login/page.jsx
@@ -84,7 +84,7 @@ const Login = () => {
                                         onChange={(e) =>
                                             setUsername(e.target.value)
                                         }
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5  "
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                                         placeholder="zain_123"
                                         required=""
                                     />
