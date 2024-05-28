d4451b7883d5b6e945c618fc5622b53a2f6eb7fa
Fikkie007 <fikkieakbar48@gmail.com>
2024-05-28 22:16:51 +0700

    kembalikan service ke sebelumnya

--- frontend/app/(admin)/dashboard/services/create/page.jsx
+++ frontend/app/(admin)/dashboard/services/create/page.jsx
@@ -8,14 +8,12 @@ import Link from "next/link";
 import { getAllCategory } from "../category/_api/api";
 
 const Page = () => {
-  const fileTypes = ["jpg", "png", "jpeg"];
-
   const createSlug = (serviceName) => {
     const slug = serviceName.replace(/[^a-zA-Z0-9]/gi, "-");
     return slug.trim().toLowerCase().replace(/-+ /g, "-");
   };
 
-  const [file, setFile] = useState("");
+  const [serviceDesc, setServiceDesc] = useState("");
   const [serviceName, setServiceName] = useState("");
   const [serviceLink, setServiceLink] = useState("");
   const [serviceSlug, setServiceSlug] = useState("");
@@ -55,7 +53,7 @@ const Page = () => {
     formData.append("link_wa", serviceLink);
     formData.append("category_id", category);
     formData.append("price", price);
-    formData.append("image", file);
+    formData.append("deskripsi", serviceDesc);
 
     try {
       const response = await axios.post(
@@ -94,17 +92,17 @@ const Page = () => {
         <div className="grid grid-flow-col gap-6 w-fit ">
           <div className="p-4 bg-white bg-opacity-45 rounded-xl shadow-lg">
             <svg
-              className="flex-shrink-0 w-10 h-10 drop-shadow-lg shadow-black text-[#3f8ac7] "
+              class="flex-shrink-0 w-10 h-10 drop-shadow-lg shadow-black text-[#3f8ac7] "
               fill="none"
-              strokeWidth="1.5"
+              stroke-width="1.5"
               stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                stroke-linecap="round"
+                stroke-linejoin="round"
                 d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
               ></path>
             </svg>
@@ -154,7 +152,7 @@ const Page = () => {
                 />
               </div>
             </div>
-            <div className="grid md:grid-flow-col max-w-4xl gap-5 ">
+            <div class="grid md:grid-flow-col max-w-4xl gap-5 ">
               <div className="relative z-0 w-full mb-5 group">
                 <label className="block mb-2 text-sm font-medium text-gray-900">
                   Price
@@ -181,7 +179,7 @@ const Page = () => {
                   </Link>
                 </div>
                 <select
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   name="category_id"
                   value={category}
                   onChange={(e) => setcategory(e.target.value)}
@@ -211,15 +209,18 @@ const Page = () => {
                 />
               </div>
             </div>
-            <div className="relative z-0 max-w-md mb-5">
+            <div class="relative z-0 mb-5">
               <label className="block mb-2 text-sm font-medium text-gray-900">
-                Image
+                Description
               </label>
-              <FileUploader
-                handleChange={handleChange}
-                name="image"
-                types={fileTypes}
-              />
+              <textarea
+                id="deskripsi"
+                value={serviceDesc}
+                onChange={(e) => setServiceDesc(e.target.value)}
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+                placeholder="Deskripsi Dari Service"
+              ></textarea>
             </div>
             <button
               type="submit"
