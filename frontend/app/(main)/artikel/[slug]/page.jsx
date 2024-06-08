e8490d96e8b0712bb14ae369dc4a18690984d490
Fikkie007 <fikkieakbar48@gmail.com>
2024-06-08 21:01:28 +0700

    - filter rekomendasi artikel berdasarkan artikel yang sedang dibuka

--- frontend/app/(main)/artikel/[slug]/page.jsx
+++ frontend/app/(main)/artikel/[slug]/page.jsx
@@ -11,6 +11,11 @@ import Link from "next/link";
 const Page = ({ params }) => {
   const [getArtikel, setGetArtikel] = useState("");
   const [groupData, setGroupData] = useState([]);
+  const [slicedRandomProducts, setSlicedRandomProducts] = useState([]);
+  const [slicedRandomProductsMobile, setslicedRandomProductsMobile] = useState(
+    []
+  );
+  const [loading, setLoading] = useState(false);
 
   function join(date, options, separator) {
     function format(option) {
@@ -46,11 +51,19 @@ const Page = ({ params }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const data = await showArtikel(params.slug);
         const res = await getAllArtikel(data.category_id);
         console.log("🚀 ~ fetchData ~ res:", res);
+        const filteredArticles = res.filter((article) => {
+          return article.id === params.id;
+        });
         setGroupData(res);
         setGetArtikel(data);
+        const shuffledProducts = useShuffleArray(filteredArticles);
+        setSlicedRandomProducts(shuffledProducts.slice(0, 3));
+        setslicedRandomProductsMobile(shuffledProducts.slice(0, 2));
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching artikel:", error);
       }
@@ -58,9 +71,6 @@ const Page = ({ params }) => {
 
     fetchData();
   }, []);
-
-  const shuffledProducts = useShuffleArray(groupData);
-  const slicedRandomProducts = shuffledProducts.slice(0, 3);
 
   return (
     <>
@@ -109,59 +119,53 @@ const Page = ({ params }) => {
           </div>
         </div>
         <div className="mx-32 my-10">
-        <p className="text-[#FFB62B] font-semibold text-lg md:text-xl lg:text-3xl my-5 mt-2">
-          Similar Articles{" "}
-        </p>
-        <div className="flex justify-between">
-          {slicedRandomProducts.map((item, itemIndex) => (
-            <div className="max-w-sm bg-white border  border-gray-200 rounded-lg shadow">
-              <Link
-                href={`/artikel/${item.slug}`}
-                className="w-96"
-                key={itemIndex}
-              >
-                <img
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/public/artikel/${item.image}`}
-                  alt={`${item.judul}`}
-                  className="w-96 h-80 rounded-t-lg"
-                />
-              </Link>
-              <div className="p-5">
-                <div className="px-2 py-4">
-                  <Link
-                    href={`/artikel/${item.slug}`}
-                    className="w-96"
-                    key={itemIndex}
-                  >
-                    <div className="mb-2 text-2xl font-bold tracking-tight text-wrap break-words text-gray-900">
-                      {item.judul}
-                    </div>
-                  </Link>
-                  <p className=" font-normal text-gray-700 dark:text-gray-400">
-                    {item.description ?? "-"}
-                  </p>
-                </div>
-                <span
-                  className={`inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 ml-1 mb-2 cursor-pointer
-                  }`}
-                >
-                  #{item.category}
-                </span>
-                <Link
-                  href={`/artikel/${item.slug}`}
-                  className="mt-4 block items-center px-3 py-2 w-32 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          <p className="text-[#FFB62B] font-semibold text-lg md:text-xl lg:text-3xl my-5 mt-2">
+            Similar Articles{" "}
+          </p>
+          <div className="flex justify-between">
+            {!loading && slicedRandomProducts.length === 0 ? (
+              <p>Artikel Lain Masih Belum Ada</p>
+            ) : (
+              slicedRandomProducts.map((item, itemIndex) => (
+                <div
+                  className="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
                   key={itemIndex}
                 >
-                  Read More
-                </Link>
-              </div>
-            </div>
-          ))}
+                  <Link href={`/artikel/${item.slug}`} className="w-96">
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/public/artikel/${item.image}`}
+                      alt={item.judul}
+                      className="w-96 h-80 rounded-t-lg"
+                    />
+                  </Link>
+                  <div className="p-5">
+                    <div className="px-2 py-4">
+                      <Link href={`/artikel/${item.slug}`} className="w-96">
+                        <div className="mb-2 text-2xl font-bold tracking-tight text-wrap break-words text-gray-900">
+                          {item.judul}
+                        </div>
+                      </Link>
+                      <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {item.description ?? "-"}
+                      </p>
+                    </div>
+                    <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 ml-1 mb-2 cursor-pointer">
+                      #{item.category}
+                    </span>
+                    <Link
+                      href={`/artikel/${item.slug}`}
+                      className="mt-4 block items-center px-3 py-2 w-32 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                      key={itemIndex}
+                    >
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
-      </div>
-
-     
 
       {/* Mobile View */}
       <div className="block tablet:hidden container">
@@ -187,54 +191,49 @@ const Page = ({ params }) => {
           </div>
         </div>
         <div className="ml-5 my-10">
-        <p className="text-[#FFB62B] font-semibold text-[33px] my-5 mt-2">
-          Similar Articles{" "}
-        </p>
-        <div className="flex justify-between">
-          {slicedRandomProducts.map((item, itemIndex) => (
-            <div className="max-w-[177px] bg-white border  border-gray-200 rounded-lg shadow w-full">
-              <Link
-                href={`/artikel/${item.slug}`}
-                className="w-full"
-                key={itemIndex}
-              >
-                <img
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/public/artikel/${item.image}`}
-                  alt={`${item.judul}`}
-                  className="w-full h-28 rounded-t-lg"
-                />
-              </Link>
-              <div className="p-2">
-                <div className="px-2 py-2">
-                  <Link
-                    href={`/artikel/${item.slug}`}
-                    className="w-full"
-                    key={itemIndex}
-                  >
-                    <div className="mb-2 text-sm font-bold tracking-tight text-wrap break-words text-gray-900">
-                      {item.judul}
-                    </div>
-                  </Link>
-                  
-                </div>
-                <span
-                  className={`inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 ml-1 mb-2 cursor-pointer
-                  }`}
-                >
-                  #{item.category}
-                </span>
-                <Link
-                  href={`/artikel/${item.slug}`}
-                  className="mt-1 px-1 block items-center text-sm font-bold text-blue-600 hover:underline "
+          <p className="text-[#FFB62B] font-semibold text-[33px] my-5 mt-2">
+            Similar Articles{" "}
+          </p>
+          <div className="flex justify-between">
+            {!loading && slicedRandomProductsMobile.length === 0 ? (
+              <p>Artikel Lain Masih Belum Ada</p>
+            ) : (
+              slicedRandomProductsMobile.map((item, itemIndex) => (
+                <div
+                  className="max-w-[177px] bg-white border border-gray-200 rounded-lg shadow w-full"
                   key={itemIndex}
                 >
-                  Read More
-                </Link>
-              </div>
-            </div>
-          ))}
+                  <Link href={`/artikel/${item.slug}`} className="w-full">
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/public/artikel/${item.image}`}
+                      alt={item.judul}
+                      className="w-full h-28 rounded-t-lg"
+                    />
+                  </Link>
+                  <div className="p-2">
+                    <div className="px-2 py-2">
+                      <Link href={`/artikel/${item.slug}`} className="w-full">
+                        <div className="mb-2 text-sm font-bold tracking-tight text-wrap break-words text-gray-900">
+                          {item.judul}
+                        </div>
+                      </Link>
+                    </div>
+                    <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 ml-1 mb-2 cursor-pointer">
+                      #{item.category}
+                    </span>
+                    <Link
+                      href={`/artikel/${item.slug}`}
+                      className="mt-1 px-1 block items-center text-sm font-bold text-blue-600 hover:underline"
+                      key={itemIndex}
+                    >
+                      Read More
+                    </Link>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
