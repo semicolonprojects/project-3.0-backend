baf38d929e38e130038ce34e46836fd57a4dc550
Fikkie007 <fikkieakbar48@gmail.com>
2024-07-03 23:19:16 +0700

    - fix card service, error not found

--- frontend/app/(admin)/dashboard/CardInfo.jsx
+++ frontend/app/(admin)/dashboard/CardInfo.jsx
@@ -7,74 +7,74 @@ import { getArtikel } from "../../api/v2/artikel/getArtikel";
 import Link from "next/link";
 
 const CardInfo = () => {
-  const [countProduct, setCountProduct] = useState("");
-  const [countService, setCountService] = useState("");
-  const [countArtikel, setCountArtikel] = useState("");
+    const [countProduct, setCountProduct] = useState("");
+    const [countService, setCountService] = useState("");
+    const [countArtikel, setCountArtikel] = useState("");
 
-  useEffect(() => {
-    const countProduct = async () => {
-      try {
-        const product = await getProducts();
-        setCountProduct(product.length);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    useEffect(() => {
+        const countProduct = async () => {
+            try {
+                const product = await getProducts();
+                setCountProduct(product.length);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    const countService = async () => {
-      try {
-        const service = await getServices();
-        setCountService(service.length);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+        const countService = async () => {
+            try {
+                const service = await getServices();
+                setCountService(service.length);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    const countArtikel = async () => {
-      try {
-        const artikel = await getArtikel();
-        setCountArtikel(artikel.length);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+        const countArtikel = async () => {
+            try {
+                const artikel = await getArtikel();
+                setCountArtikel(artikel.length);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-    countProduct();
-    countService();
-    countArtikel();
-  }, []);
+        countProduct();
+        countService();
+        countArtikel();
+    }, []);
 
-  return (
-    <div className="grid grid-flow-col gap-2 py-20 pb-5">
-      <Link
-        href="/dashboard/products"
-        className=" bg-white w-72 p-4 rounded-md shadow-sm"
-      >
-        <div className="grid grid-flow-row">
-          <p className="font-bold text-2xl">Products</p>
-          <p className="font-extrabold text-4xl">{countProduct}</p>
+    return (
+        <div className="grid grid-flow-col gap-2 py-20 pb-5">
+            <Link
+                href="/dashboard/products"
+                className=" bg-white w-72 p-4 rounded-md shadow-sm"
+            >
+                <div className="grid grid-flow-row">
+                    <p className="font-bold text-2xl">Products</p>
+                    <p className="font-extrabold text-4xl">{countProduct}</p>
+                </div>
+            </Link>
+            <Link
+                href="/dashboard/services"
+                className=" bg-white w-72 p-4 rounded-md shadow-sm"
+            >
+                <div className="grid grid-flow-row">
+                    <p className="font-bold text-2xl">Services</p>
+                    <p className="font-extrabold text-4xl">{countService}</p>
+                </div>
+            </Link>
+            <Link
+                href="/dashboard/artikel"
+                className=" bg-white w-72 p-4 rounded-md shadow-sm"
+            >
+                <div className="grid grid-flow-row">
+                    <p className="font-bold text-2xl">Artikel</p>
+                    <p className="font-extrabold text-4xl">{countArtikel}</p>
+                </div>
+            </Link>
         </div>
-      </Link>
-      <Link
-        href="/dashboard/service"
-        className=" bg-white w-72 p-4 rounded-md shadow-sm"
-      >
-        <div className="grid grid-flow-row">
-          <p className="font-bold text-2xl">Services</p>
-          <p className="font-extrabold text-4xl">{countService}</p>
-        </div>
-      </Link>
-      <Link
-        href="/dashboard/artikel"
-        className=" bg-white w-72 p-4 rounded-md shadow-sm"
-      >
-        <div className="grid grid-flow-row">
-          <p className="font-bold text-2xl">Artikel</p>
-          <p className="font-extrabold text-4xl">{countArtikel}</p>
-        </div>
-      </Link>
-    </div>
-  );
+    );
 };
 
 export default CardInfo;
