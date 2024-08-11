37a66fa6493b8b7c10ebc6ebb39431d02b40f9be
Breyhan-F9 <breyhan20@gmail.com>
2024-08-11 19:23:34 +0700

    refactor: changing responsive on 1536 x 864 screen resolutions

--- frontend/app/components/Sidebar.jsx
+++ frontend/app/components/Sidebar.jsx
@@ -131,13 +131,13 @@ const Sidebar = () => {
                                                 height={60}
                                                 width={60}
                                                 alt="..."
-                                                className="rotate-90 ml-6 desktop-md:w-24 "
+                                                className="rotate-90 ml-6 desktop-lg:w-24 "
                                                 unoptimized
                                             />
                                         </div>
                                     </Link>
                                 </div>
-                                <div className=" mt-0 grid grid-flow-row font-bold text-5xl desktop-sm:text-5xl desktop-md:text-7xl desktop-md:mt-24 px-20 text-[#4A89B0]  gap-y-11 desktop-sm:gap-y-20">
+                                <div className=" mt-0 grid grid-flow-row font-bold text-5xl desktop-sm:text-5xl desktop-lg:text-7xl desktop-lg:mt-24 px-20 text-[#4A89B0]  gap-y-11 desktop-sm:gap-y-20">
                                     <Link href="/services">
                                         <button className="text-left hover:text-[#FFB62B]">
                                             Our Services
